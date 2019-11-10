7bc7dbdcf7d36f75cab90b226b278b9a654ddcea
marpivPraca <mariuszpiwowarski3@gmail.com>
2019-11-10 21:00:57 +0100

    Added automatic reading of file size

--- CpyPlik.c
+++ CpyPlik.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#define MAX 512
+#define MAX 47
 
 int main(int argc, char* argv[]){
 	char* buf;
 	int desc_zrod, desc_cel;
 	int lbajt;
+	int  rozm;
 
 	if (argc<3){
 		fprintf(stderr, "Za malo argumentow. Uzyj: \n");
@@ -19,16 +20,25 @@ int main(int argc, char* argv[]){
 		perror("Blad otwarcia pliku zrodlowego");
 		exit(1);
 	}
-	
+
+	rozm = lseek(desc_zrod, 0, SEEK_END);
+	if (rozm == -1){
+		perror("Blad lseek");
+		exit(1);
+	}
+
+	lseek(desc_zrod, 0, SEEK_SET);
+        
 	desc_cel = creat(argv[2], 0640);
 	if (desc_cel == -1){
 		perror("Blad otwarcia pliku docelowego.");
 		exit(1);
 	}
 	
-	buf = (char*) malloc(MAX);
+	buf = (char*) malloc(rozm);
+	printf("Rozmiar pliku  zrodlowego : %ld \n", rozm);
 
-	while ((lbajt = read(desc_zrod, buf, MAX)) > 0){
+	while ((lbajt = read(desc_zrod, buf, rozm)) > 0){
 		if (write(desc_cel, buf, lbajt) == -1){
 			perror("Blad zapisu pliku docelowego.");
 			exit(1);
